cab80cf7b9368de2539f43653837c370de5fa400
mconsiglio06 <m.consiglio06@gmail.com>
2026-05-11 12:03:43 +0200

    Aggiunta struttura dati per utenti; modifica messaggio eccezione data

--- src/cinemax/DateFormatException.java
+++ src/cinemax/DateFormatException.java
@@ -2,6 +2,6 @@ package cinemax;
 
 public class DateFormatException extends RuntimeException {
     public DateFormatException() {
-        super("ERROR! The given date's format is not accepted.");
+        super("Errore: Il formato della data è errato. Utilizzare il formato 'dd/MM/yyyy'.");
     }
 }
